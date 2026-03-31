968f39b35ac99f0868f09c4249b12faf71769c53
saimaali-tech <saimaalisaima313@gmail.com>
2026-04-01 02:53:00 +0500

    Implemented add task functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useState } from "react";
 
 function App() {
-    const [task, setTask] = useState("");
+
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const addTask = () => {
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
   return (
     <div>
       <h1>DevOps Daily Tracker 🚀</h1>
-      <input placeholder="Enter task..." />
-      <button>Add</button>
+      <input 
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Enter task..." 
+      />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }
