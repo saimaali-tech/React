b546a1f1f4ecbe26c6ba71d7388695a2bcd3b319
saimaali-tech <saimaalisaima313@gmail.com>
2026-04-01 02:58:51 +0500

    Added delete task functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import { useState } from "react";
 
@@ -6,30 +5,35 @@ function App() {
 
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
+
   const addTask = () => {
     setTasks([...tasks, task]);
     setTask("");
   };
 
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
       <h1>DevOps Daily Tracker 🚀</h1>
+
       <input 
         value={task}
         onChange={(e) => setTask(e.target.value)}
         placeholder="Enter task..." 
       />
+
       <button onClick={addTask}>Add</button>
       <ul>
         {tasks.map((t, index) => (
-          <li key={index}>{t}</li>
+          <li key={index}>
+            {t}
+            <button onClick={() => deleteTask(index)}> ❌ </button>
+          </li>
         ))}
       </ul>
-      <ul>
-  {tasks.map((t, index) => (
-    <li key={index}>{t}</li>
-  ))}
-</ul>
 
     </div>
   );
