d0dbd441ac3bd7196f05487b7982be64534a2bee
saimaali-tech <saimaalisaima313@gmail.com>
2026-04-01 02:45:26 +0500

    Added state management for input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+    const [task, setTask] = useState("");
   return (
     <div>
       <h1>DevOps Daily Tracker 🚀</h1>
